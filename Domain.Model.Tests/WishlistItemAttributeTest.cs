c198d956db348b22a7bf90de1082c15d5c83d831
VASCONCELOS NEVES João Pedro <joaopedro.vasconcelosneves@altran.com>
2020-04-22 11:15:05 +0100

    Implement wishlist item and attribute tests

---
+++ Domain.Model.Tests/WishlistItemAttributeTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Domain.Model.Tests
+{
+    [TestClass]
+    public class WishlistItemAttributeTest
+    {
+        WishlistItemAttribute attribute;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            this.attribute = new WishlistItemAttribute();
+        }
+
+        [TestMethod()]
+        public void WishlistItemAttribute_GettersAndSetters()
+        {
+            this.attribute.Key = "Color";
+            this.attribute.Value = "Red";
+            this.attribute.WishlistItemId = 2121;
+            this.attribute.DateCreated = new DateTime(2020, 4, 10);
+            this.attribute.DateUpdated = new DateTime(2020, 4, 20);
+
+            var key = this.attribute.Key;
+            var value = this.attribute.Value;
+            var itemID = this.attribute.WishlistItemId;
+            var dateCreated = this.attribute.DateCreated;
+            var dateUpdated = this.attribute.DateUpdated;
+
+            Assert.AreEqual("Color", key);
+            Assert.AreEqual("Red", value);
+            Assert.AreEqual(2121, itemID);
+            Assert.AreEqual(new DateTime(2020, 4, 10), dateCreated);
+            Assert.AreEqual(new DateTime(2020, 4, 20), dateUpdated);
+        }
+    }
+}
---
+++ Domain.Model.Tests/WishlistItemTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Domain.Model.Tests
+{
+    [TestClass]
+    public class WishlistItemTest
+    {
+        WishlistItem item;
+        List<WishlistItemAttribute> attributes;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            this.attributes = new List<WishlistItemAttribute>();
+            this.item = new WishlistItem();
+        }
+
+        [TestMethod]
+        public void WishlistItem_GettersAndSetters()
+        {
+            this.item.Price = 20;
+            this.item.Name = "Item";
+            this.item.Code = "Item12";
+            this.item.WishlistId = 1;
+            this.item.Id = 1;
+            this.item.Attributes = this.attributes;
+            this.item.DateCreated = new DateTime(2018, 8, 11);
+            this.item.DateUpdated = new DateTime(2019, 10, 26);
+
+            double price = this.item.Price;
+            string name = this.item.Name;
+            string code = this.item.Code;
+            int id = this.item.Id;
+            int wishlistID = this.item.WishlistId;
+            List<WishlistItemAttribute> attributes = this.item.Attributes;
+            DateTime dateCreated = this.item.DateCreated;
+            DateTime dateUpdated = this.item.DateUpdated;
+
+            Assert.AreEqual(20, price);
+            Assert.AreEqual("Item12", code);
+            Assert.AreEqual(1, wishlistID);
+            Assert.AreEqual("Item", name);
+            Assert.AreEqual(1, id);
+            Assert.AreEqual(this.attributes, attributes);
+            Assert.AreEqual(new DateTime(2018, 8, 11), dateCreated);
+            Assert.AreEqual(new DateTime(2019, 10, 26), dateUpdated);
+        }
+    }
+}
